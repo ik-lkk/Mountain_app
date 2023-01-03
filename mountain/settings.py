bef0c4093b2914925bc60a79b5b148b8757503e3
user <user@mail.com>
2023-01-03 17:57:53 +0900

    pic

--- mountain/settings.py
+++ mountain/settings.py
@@ -143,7 +143,7 @@ STATIC_URL = 'static/'
 #     STATIC_DIR,
 # ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+# STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
