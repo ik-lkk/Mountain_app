7bf9f226d23ab06566ba95a4c6b4e4fc9a3688dc
user <user@mail.com>
2023-01-03 16:06:38 +0900

    syusei

--- mountain/settings.py
+++ mountain/settings.py
@@ -60,6 +60,7 @@ AUTH_USER_MODEL = 'website.Users'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
