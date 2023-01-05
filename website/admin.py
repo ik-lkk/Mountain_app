787163938f683d86c87ad4ea468aa38b1cf181ae
user <user@mail.com>
2023-01-05 18:16:39 +0900

    contact

--- website/admin.py
+++ website/admin.py
@@ -1,11 +1,13 @@
 from django.contrib import admin
-from .models import Mountain,Themes,Comments,Users
+from .models import Mountain, Themes, Comments, Users, ContactModel
 # Register your models here.
 
 admin.site.register(Mountain)
 admin.site.register(Users)
 admin.site.register(Comments)
 admin.site.register(Themes)
+admin.site.register(ContactModel)
+
 
 
 
